07b7b9c3fff6ce4feaeea4ce80aa086440101066
Suhiru Benjamin De Silva <66971839+Suhiru@users.noreply.github.com>
2020-10-13 22:27:18 +0530

    Quota

--- src/app/sales-list/sales-list.component.ts
+++ src/app/sales-list/sales-list.component.ts
@@ -3,7 +3,7 @@ import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-list',
-  templateUrl: './sales-list.component.html',
+  templateUrl: './sales-list-bootstrap.component.html',
   styleUrls: ['./sales-list.component.css']
 })
 export class SalesListComponent implements OnInit {
@@ -11,12 +11,12 @@ export class SalesListComponent implements OnInit {
   salesPersonList: SalesPerson[] = [
 
     new SalesPerson("Suhiru", "Benjamin", "suhiru@123.com", 52000),
-    new SalesPerson("Dulip", "Kumara", "dulip@123.com", 62000),
+    new SalesPerson("Dulip", "Kumara", "dulip@123.com", 12000),
     new SalesPerson("Chamod", "Lal", "chamod@123.com", 700000),
-    new SalesPerson("Mahima", "Induwara", "mahima@123.com", 8000),
+    new SalesPerson("Mahima", "Induwara", "mahima@123.com", 80000),
     new SalesPerson("Kushan", "Ariyadasa", "kushan@123.com", 45000),
-    new SalesPerson("Anais", "Leal", "anais@123.com", 55000),
-    new SalesPerson("Tyronne", "Benjamin", "tyronne@123.com", 25000),
+    new SalesPerson("Anais", "Leal", "anais@123.com", 65000),
+    new SalesPerson("Tyronne", "Benjamin", "tyronne@123.com", 95000),
 
 ];
 
